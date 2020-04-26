a85853ba50c6c29f78b054be8e949f29785b1f31
vinicius <vincius.cavalcanti16@gmail.com>
2020-04-26 12:05:38 -0300

    avatar download

--- sql/utfparking_public_role.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.role (id, description, name) VALUES (2, 'Perfil Administrador', 'ROLE_ADMIN');
-INSERT INTO public.role (id, description, name) VALUES (3, 'Perfil Operador', 'ROLE_OPERATOR');
-INSERT INTO public.role (id, description, name) VALUES (1, 'Perfil usuário', 'ROLE_USER');
